2cd2d8dd3afa13be712ef22ee91bf3426f1424c6
Jonas Willian <jwnarancity@gmail.com>
2022-08-24 10:54:53 -0300

    Add files via upload
    Criei esse e-commerce de uma pizzaria através do desafio do Bonieky, o projeto me pareceu bastante complexo no início tanto que comecei e não terminei, passaram-se meses até que estudei e decidi retomar o projeto e finalmente acabei de concluí-lo!
    No projeto foram utilizados HTNL, CSS E JavaScript.

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:25.50, sizes:['100g', '530g', '860g'], description:'Pizza de Mussarela, uma das mais pedidas da casa sendo que o diferencial são os produtos de qualidades '},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:28.00, sizes:['320g', '530g', '860g'], description:'Pizza tradicional de calabresa, seu diferencial são produtos de qualidades'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:27.45, sizes:['320g', '530g', '860g'], description:'Pizza 4 queijos, seu diferencial são os produtos que por sinal são todos de primeira linha'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:29.77, sizes:['320g', '530g', '860g'], description:'Pizza Americana, uma tradição da casa que por sinal é a mais pedida por nossos clientes'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:31.43, sizes:['320g', '530g', '860g'], description:'Temos aqui a mais nova queridinha da criançada, nossa pizza doce sabor sovete'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:28.55, sizes:['320g', '530g', '860g'], description:'Se está á procura de algo diferente do habitual sugerimos a Pizza da casa, essa delícia você só encontrará aqui '},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:32.99, sizes:['320g', '530g', '860g'], description:'Novidade no cardápio são ás Pizzas Doces, dentre elas temos a de chocolate que tem feito sucesso entre nossos clientes'}
+];
